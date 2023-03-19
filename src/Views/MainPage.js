3c26eadeda0242e4edcacbbeb94dd118f3ac5725
kouzber123 <48252892+kouzber123@users.noreply.github.com>
2023-03-19 18:05:23 +0200

    Feature login with keycloak

--- src/Views/MainPage.js
+++ src/Views/MainPage.js
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import MainView from "../Components/Main/MainView";
-
+import UserService from "../Service/userservice";
 const MainPage = () => {
+  const [user, setuser] = useState("");
 
-    return (
-        <>
-            <h1>Main</h1>
-            <MainView/>
-        </>
-    )
-}
+  useEffect(() => {
+    setuser(UserService.getUsername());
+  }, []);
+  return (
+    <>
+      <h1>Main</h1>
 
-export default MainPage; 
+      <div>
+        <p className="lead">Please authenticate yourself!</p>
+        <p>
+          <button className="btn btn-lg btn-warning" onClick={() => UserService.doLogin()}>
+            Login
+          </button>
+        </p>
+      </div>
+
+      {UserService.kc && (
+        <div>
+          <h1>Logged in</h1>
+
+          <p>{() => UserService.getUsername()}</p>
+          <p>hello user!</p>
+        </div>
+      )}
+
+      <MainView />
+    </>
+  );
+};
+
+export default MainPage;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AppContext from "./Context/AppContext";
+import axios from "axios";
+import UserService from "./Service/userservice";
+
+const _axios = axios.create();
+_axios.interceptors.request.use(config => {
+  if (UserService.isLoggedIn()) {
+    const cb = () => {
+      config.headers.Authorization = `Bearer ${UserService.getToken()}`;
+      return Promise.resolve(config);
+    };
+
+    return UserService.updateToken(cb);
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,7 +27,22 @@ root.render(
     </AppContext>
   </React.StrictMode>
 );
+UserService.initKeycloak();
+// {}UserService.initKeycloak &&  )
 
+// const token = () => {
+//   const result = UserService.doLogin();
+//   console.log(result);
+// };
+// token();
+// const token = document.cookie
+//   .split(";")
+//   .find(row => row.startsWith("KEYCLOAK_SESSION_LEGACY"))
+//   .split("=")[1];
+
+// Set the token to local storage
+
+// localStorage.setItem("token", x);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
